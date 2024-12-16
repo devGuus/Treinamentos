f9e109e001459a9bd905eaffec9c994e1a26a368
Gustavo Xavier <93723573+devGuus@users.noreply.github.com>
2024-12-16 02:20:38 -0300

    Update quiz.py

--- Arquivos Python/quiz.py
+++ Arquivos Python/quiz.py
@@ -1,37 +1,40 @@
 print("Bem vindo ao quiz em de python para iniciantes!\nDeseja iniciar? {S/N}")
 iniciarQuiz = input()
-score = 0
-qtd_perguntas = 2
+respostas_usuario = []
+# Coloque as respostas em ordem aqui
+respostas_corretas = ["b", "c"]
+qtd_perguntas = len(respostas_corretas) # quantidade de perguntas
 
-def analisarResposta(resposta, correta):
-    if resposta == correta:
-        return "correta"
-    else:
-        return "errada"
+# Analisa se a resposta está correta e retorna a quantidade de acertos
+def analisarResposta(respostas_user, lista_resp):
+    score = 0
+    for user, correta in zip(respostas_user, lista_resp):
+        if user == correta:
+            score += 1
+    return score
 
-def contarPontos(resposta):
-    if resposta == "correta":
-        return 1
-    else:
-        return 0
-
+# Iniciar o quiz
 if iniciarQuiz != "S" and iniciarQuiz != "s":
+    print("Encerrando Quiz...")
     quit()
+
 # Primeira pergunta
 print("Perfeito! Vamos para primeira pergunta\nQual comando utilizamos para imprimir alguma informação na tela ou terminal?\n(A) printf()\n(B) print()\n(C) system_out_println()\n(D) Console.WriteLine()")
-questao1 = input("Resposta: ")
-resposta1 = "B"
-
+resposta = input("Resposta: ").strip().lower() # Garante que a resposta fique minuscula para conferir
+respostas_usuario.append(resposta) # Aplicando a resposta na lista de respostas
 
 # Segunda pergunta
 print("Qual destas estruturas de controle permite executar um bloco de código repetidamente?\n(A) if\n(B) def\n(C) for\n(D) import")
-questao2 = input("Resposta: ")
+resposta = input("Resposta: ").strip().lower() # Garante que a resposta fique minuscula para conferir
+respostas_usuario.append(resposta) # Aplicando a resposta na lista de respostas
 
-if questao2 == "C" or questao2 == "c":
-    print("Correto")
-    score += 1
-else:
-    print("Errada")
+# Contando os pontos
+pontuacao_total = analisarResposta(respostas_usuario, respostas_corretas)
 
 # score de acertos
-print(f"Quantidade de acertos: {score}/{qtd_perguntas}")
+if pontuacao_total == qtd_perguntas:
+    print(f"Parabéns, você acertou todas as respostas!!🎉\nProntuação: {pontuacao_total}/{qtd_perguntas}") # Acertou Todas
+elif pontuacao_total > 0:
+    print(f"Bom trabalho! Você acertou algumas perguntas. Continue praticando!\nProntuação: {pontuacao_total}/{qtd_perguntas}") # Acertou algumas
+else:
+    print(f"Não desista, sei que na próxima você vai se sair melhor!\nProntuação: {pontuacao_total}/{qtd_perguntas}") # Errou todas
